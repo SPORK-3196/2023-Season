caf5397cb1e1dd4f463b54756c3f6c43c8c1d9e1
CorbinHass <12270068@pinelakeprep.org>
2023-02-05 13:03:19 -0500

    Claw Subsystem

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -1,5 +1,22 @@
 package frc.robot.subsystems;
 
-public class Claw {
-    
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Claw extends SubsystemBase{
+    public Solenoid piston1 = new Solenoid(PneumaticsModuleType.REVPH, 10);
+
+    public Claw(){
+    }
+
+    public void TogglePiston(){
+        piston1.toggle();
+    }
+    public void extendPiston(){
+        piston1.set(true);
+    }
+    public void retractPiston(){
+        piston1.set(false);
+    }
 }
